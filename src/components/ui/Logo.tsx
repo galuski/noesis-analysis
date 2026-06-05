a63de541681a8c70ba412c673fc886a32a30d00a
Gal Luski <109075193+galuski@users.noreply.github.com>
2026-06-05 16:01:18 +0300

    Fix logo SVG display issue ./

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -4,11 +4,12 @@ import Image from 'next/image';
 // ייבוא קובץ העיצוב הייעודי של הלוגו
 import styles from '@/app/styles/components/ui/Logo.module.css';
 
+
 export default function Logo() {
   return (
     <Link href="/" className={styles.logoLink}>
       <Image 
-        src="/logo.svg" 
+        src="./logo.svg" 
         alt="My Company Logo" 
         width={80} 
         height={80} 
@@ -17,5 +18,5 @@ export default function Logo() {
       />
     </Link>
   );
-  
+
 }
